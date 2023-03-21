f8b581254297dfeaccbe70c4bdb1b3f0841069be
shashikaMahanama <91842292+shashikaMahanama@users.noreply.github.com>
2023-03-21 18:54:30 +0530

    initial commit

---
+++ backend/routes/user.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const router = require('./workouts')
+const route=express.Router()
+
+//import controller function
+const {signupUser,loginUser} =require('../controllers/usercontroller')
+
+
+
+//login route (this is post meth cause it send data to server)
+router.post('/login',loginUser)
+
+
+//signup route
+router.post('/signup',signupUser)
+
+module.exports=router
